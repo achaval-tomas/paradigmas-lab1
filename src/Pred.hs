b1a00e628c8c06c515d81cb7b6306ce92156ad19
Tomás Peyronel <tpeyronel@gmail.com>
2024-03-23 16:15:12 -0300

    implementar cambiar

--- src/Pred.hs
+++ src/Pred.hs
@@ -1,14 +1,28 @@
-module Pred (
-  Pred,
-  cambiar, anyDib, allDib, orP, andP, falla
-) where
+module Pred
+  ( Pred,
+    allDib,
+    andP,
+    anyDib,
+    cambiar,
+    falla,
+    orP,
+  )
+where
 
+import Dibujo (Dibujo, apilar, encimar, espejar, figura, foldDib, juntar, rot45, rotar)
+
+-- `Pred a` define un predicado sobre figuras básicas. Por ejemplo,
+-- `(== Triangulo)` es un `Pred TriOCuat` que devuelve `True` cuando la
+-- figura es `Triangulo`.
 type Pred a = a -> Bool
 
--- Dado un predicado sobre básicas, cambiar todas las que satisfacen
--- el predicado por la figura básica indicada por el segundo argumento.
-cambiar = undefined
-
+-- Dado un predicado sobre figuras básicas, cambiar todas las que satisfacen
+-- el predicado por el resultado de llamar a la función indicada por el
+-- segundo argumento con dicha figura.
+-- Por ejemplo, `cambiar (== Triangulo) (\x -> Rotar (Figura x))` rota
+-- todos los triángulos.
+cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
+cambiar p f = foldDib (\x -> if p x then f x else figura x) rotar rot45 espejar juntar apilar encimar
 
 -- Alguna básica satisface el predicado.
 anyDib = undefined
