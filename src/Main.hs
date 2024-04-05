adc61774ba6f40f3e7c4f791bd92a5e45003c5ce
Tomás Peyronel <tpeyronel@gmail.com>
2024-04-05 18:23:28 -0300

    permitir elegir dibujo al usar -l

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,16 @@
 module Main (main) where
 
 import Control.Monad (when)
--- import Dibujos.Ejemplo (ejemploConf)
+import Dibujos.Escher (escherConf)
 import Dibujos.Feo (feoConf)
 import Dibujos.Grilla (grillaConf)
-import Dibujos.Escher (escherConf)
--- import Dibujos.Cuadrados(cuadConf)
 import FloatingPic (Conf (..))
 import Interp (initial)
 import InterpHaha (ConfH, initialH', simpleHaha)
 import InterpSVG (ConfSVG, initialSVG', simpleSVG)
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
+import System.IO (hFlush, stdout)
 
 -- Lista de configuraciones de los dibujos
 configs :: [Conf]
@@ -41,9 +40,25 @@ main = do
   when (length args > 2 || null args) $ do
     putStrLn "Sólo puede elegir un dibujo. Para ver los dibujos use -l ."
     exitFailure
-  when (head args == "-l") $ do
-    putStrLn "Los dibujos disponibles son:"
-    mapM_ (putStrLn . name) configs
+  when (head args `elem` ["-l", "--lista"]) $ do
+    let loop = do
+          putStrLn "Los dibujos disponibles son:"
+          mapM_ (putStrLn . (" - " ++) . name) configs
+
+          putStr "Elija uno o escriba 'q' para salir: "
+          hFlush stdout
+
+          c <- getLine
+
+          when (c == "q") exitSuccess
+          if c `elem` map name configs
+            then return c
+            else do
+              putStrLn "Error: el dibujo ingresado es incorrecto."
+              loop
+    c <- loop
+    putStrLn $ "Mostrando: " ++ c
+    initial' configs c
     exitSuccess
   when (head args == "-a" && not (null $ tail args)) $ do
     initialH' configsH (args !! 1)
